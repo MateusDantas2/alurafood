f5d005d78cd05860834000ed01dde8677e794593
Mateus Nascimento <mateus.nascimento@omnilink.com.br>
2024-09-23 21:34:07 -0300

    Finaliza serviço de pagamento

--- src/main/java/br/com/alurafood/pagamentos/domain/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.alurafood.pagamentos.domain.repository;
-
-import br.com.alurafood.pagamentos.domain.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * @author Mateus Dantas
- */
-public interface PaymentRepository extends JpaRepository<Payment, Long> {}
